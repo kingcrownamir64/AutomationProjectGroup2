e8fb60c59d4e1c2f16caa6cb77464aa373c97efe
kingcrownamir64 <amirhafiz08@gmail.com>
2019-03-20 19:34:08 -0400

    Pushing to manzoor

--- Amazon/src/main/java/HomePage.java
+++ Amazon/src/main/java/HomePage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
     public class HomePage extends BaseClient {
@@ -17,8 +18,11 @@ import org.openqa.selenium.support.FindBy;
         @FindBy(id="nav-your-amazon")
         private WebElement YourAmazonButton;
 
-        @FindBy(id="icp-touch-link-language")
+        @FindBy(xpath="//*[@id='nav-link-accountList'']/span[2]")
         private WebElement LanguageBar;
+
+        @FindBy (css = ".Your Account")
+        private WebElement topLink;
 
 
 
@@ -41,6 +45,16 @@ import org.openqa.selenium.support.FindBy;
         public void setLanguageBar() {
             this.LanguageBar.click();
         }
+
+        public void mouseHovering() throws Exception {
+            Actions actions = new Actions(driver);
+            actions.moveToElement(LanguageBar).perform();
+            Thread.sleep(2000);
+            actions.moveToElement(topLink).perform();
+            Thread.sleep(1500);
+            actions.click().build().perform();
+        }
+
 
     }
 
