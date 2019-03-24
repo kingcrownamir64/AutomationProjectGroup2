4d340c0547c7718091b46f321dd85843cc4cb757
kingcrownamir64 <amirhafiz08@gmail.com>
2019-03-24 14:40:51 -0400

    Added TestLoginPage

---
+++ Amazon/src/test/java/TestLoginPage.java
@@ -0,0 +1,82 @@
+import Utility2.DataReader2;
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+
+public class TestLoginPage extends BaseClient{
+
+
+    String signInPageUrl = "https://www.amazon.com/ap/signin?ie=UTF8&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.mode=checkid_setup&_encoding=UTF8&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&switch_account=signin&ignoreAuthState=1&disableLoginPrepopulate=1&ref_=ap_sw_aa";
+    LoginPage signInPage;
+
+    DataReader2 excelData = new DataReader2();
+    String path = "/Users/amir/Desktop/AutomationProjectGroup2/Amazon/src/test/Resources/ExcelFile.xls";
+
+
+    // Navigate to the sign in page url
+    @BeforeClass
+    public void initialize() {
+        this.driver.navigate().to(this.signInPageUrl);
+        this.signInPage = PageFactory.initElements(this.driver, LoginPage.class);
+        this.driver.navigate().to(this.signInPageUrl);
+    }
+
+    @Test
+    public void NoPasswordLogin() {
+        this.driver.navigate().to(this.signInPageUrl);
+        this.signInPage.enterEmail("amirhafiz08@gmail.com");
+        this.signInPage.enterPwd("");
+        this.signInPage.SignIn();
+    }
+
+    //  Tests if user can login without any credentials
+    @Test
+    public void NoCredentialLogin() {
+        this.driver.navigate().to(this.signInPageUrl);
+        this.signInPage.enterEmail("");
+        this.signInPage.enterPwd("");
+        this.signInPage.SignIn();
+    }
+
+    //  Tests if user can login without any email
+    @Test
+    public void NoEmailLogin() {
+        this.driver.navigate().to(this.signInPageUrl);
+        this.signInPage.enterEmail("");
+        this.signInPage.enterPwd("Password");
+        this.signInPage.SignIn();
+    }
+
+    //  Tests if create account button navigates to correct page
+    @Test
+    public void NavigateCreateAccount() {
+        this.driver.navigate().to(this.signInPageUrl);
+        this.signInPage.CreateAccount();
+        String CAtitle = driver.getTitle();
+        Assert.assertEquals(CAtitle, "Amazon Registration");
+    }
+
+    // Tests successful login
+    @Test
+    public void SuccessfulLogin() {
+        this.driver.navigate().to(this.signInPageUrl);
+        this.signInPage.enterEmail("amirhafiz08@gmail.com");
+        this.signInPage.enterPwd("root");
+        this.signInPage.SignIn();
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
+    }
+
+    @AfterClass
+    public void cleanUp() {
+        this.driver.quit();
+    }
+
+}
+
